28ae553d08dd79f426532f934ed1b4cf6420d296
John Ankcorn <jca@alum.mit.edu>
2014-03-03 11:12:53 -0500

    more detailed dumping for parameters

--- librarywrap/ftdiwrap.c
+++ librarywrap/ftdiwrap.c
@@ -2,6 +2,7 @@
 #include "ftdi_reference.h"
 
 static void memdump(unsigned char *p, int len, char *title);
+static void dump_context(struct ftdi_context *p);
 
 #include "ftdiwrap.h"
 
@@ -21,4 +22,30 @@ int i;
         len--;
     }
     fprintf(logfile, "\n");
+}
+
+static void dump_context(struct ftdi_context *p)
+{
+    struct libusb_context *usb_ctx;
+    fprintf(logfile, "context: %p\n", p);
+    fprintf(logfile, "    .usb_ctx: %p,\n", p->usb_ctx);
+    fprintf(logfile, "    .usb_dev: %p,\n", p->usb_dev);
+    fprintf(logfile, "    .usb_read_timeout: %d,\n", p->usb_read_timeout);
+    fprintf(logfile, "    .usb_write_timeout: %d,\n", p->usb_write_timeout);
+    //fprintf(logfile, "    .ftdi_chip_type: %d,\n", p->ftdi_chip_type);
+    fprintf(logfile, "    .baudrate: %d,\n", p->baudrate);
+    fprintf(logfile, "    .bitbang_enabled: %d,\n", p->bitbang_enabled);
+    fprintf(logfile, "    .readbuffer: %p,\n", p->readbuffer);
+    fprintf(logfile, "    .readbuffer_offset: %d,\n", p->readbuffer_offset);
+    fprintf(logfile, "    .readbuffer_remaining: %d,\n", p->readbuffer_remaining);
+    fprintf(logfile, "    .readbuffer_chunksize: %d,\n", p->readbuffer_chunksize);
+    fprintf(logfile, "    .writebuffer_chunksize: %d,\n", p->writebuffer_chunksize);
+    fprintf(logfile, "    .max_packet_size: %d,\n", p->max_packet_size);
+    fprintf(logfile, "    .interface: %d,\n", p->interface);
+    fprintf(logfile, "    .index: %d,\n", p->index);
+    fprintf(logfile, "    .in_ep: %d,\n", p->in_ep);
+    fprintf(logfile, "    .out_ep: %d,\n", p->out_ep);
+    fprintf(logfile, "    .bitbang_mode: %d,\n", p->bitbang_mode);
+    fprintf(logfile, "    .eeprom: %p,\n", p->eeprom);
+    fprintf(logfile, "    .error_str: %p,\n", p->error_str);
 }
