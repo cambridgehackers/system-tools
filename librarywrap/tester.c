c4edbd593ae01a9c52f5d5c07654dd5e8939325b
John Ankcorn <jca@alum.mit.edu>
2014-03-07 22:03:00 -0500

    comments

--- librarywrap/tester.c
+++ librarywrap/tester.c
@@ -129,20 +129,30 @@
      SHIFT_TO_UPDATE_TO_IDLE_RW(0),                         \
      SEND_IMMEDIATE
 
-#define SMAP_DUMMY     0xffffffff
-#define SMAP_SYNC      0xaa995566
-#define SMAP_NOOP      0x20000000
-#define SMAP_T1W1WORD  0x30008001
-#define SMAP_DESYNC    0x0000000d
-#define SMAP_READ_STAT 0x2800e001
-#define SMAP_FOO       0x2802c001
+#define SMAP_DUMMY           0xffffffff
+#define SMAP_SYNC            0xaa995566
+// Type 1 Packet
+//                             20000000
+//        NOP                  00
+//        READ                 28
+//        WRITE                30
+//      register                 xxx
+//      wordcount                   xxx
+// Type 2 Packet
+//                             40000000
+//
+#define SMAP_NOOP            0x20000000
+#define SMAP_T1W1WORD        0x30008001  // reg 00100, CommandRegister, Table 5-22
+#define SMAP_DESYNC          0x0000000d
+#define SMAP_READ_STATUS_REG 0x2800e001  // reg 00111, STAT, Table 5-25
+#define SMAP_READ_BOOTSTS    0x2802c001  // reg 10110, BOOTSTS, Table 5-35
 
 #define READ_STAT_REG_SINGLE                   \
      EXTENDED_COMMAND(0xc5),                  \
      IDLE_TO_SHIFT_DR,                        \
      DATAW_BYTES_LEN(19),                     \
           SWAP32(SMAP_DUMMY), SWAP32(SMAP_SYNC), SWAP32(0x40000000), \
-          SWAP32(SMAP_READ_STAT), 0x00, 0x00, 0x00,  \
+          SWAP32(SMAP_READ_STATUS_REG), 0x00, 0x00, 0x00,  \
      DATAWBIT, 0x06, 0x00,                    \
      SHIFT_TO_UPDATE_TO_IDLE(0),              \
      EXTENDED_COMMAND(0xc4),                  \
@@ -524,7 +534,7 @@ int main(int argc, char **argv)
          PULSE_CLOCK, INT16(15000000/800 - 1),  // 1.25 msec
          SET_LSB_DIRECTION(GPIO_DONE | GPIO_01),
          SET_LSB_DIRECTION(GPIO_01),
-         READ_STAT_REG(SMAP_FOO)
+         READ_STAT_REG(SMAP_READ_BOOTSTS)
     };
     static uint8_t readdata11z[] = { 0x00, 0x00, 0x00, 0x00, 0x80 };
     WRITE_READ(ctxitem0z, item17z, readdata11z);
@@ -545,7 +555,7 @@ int main(int argc, char **argv)
     static uint8_t readdata12z[] = { 0xac, 0xd6 };
     WRITE_READ(ctxitem0z, item18z, readdata12z);
 
-    static uint8_t item19z[] = { EXIT1_TO_IDLE, READ_STAT_REG(SMAP_READ_STAT) };
+    static uint8_t item19z[] = { EXIT1_TO_IDLE, READ_STAT_REG(SMAP_READ_STATUS_REG) };
     static uint8_t readdata13z[] = { 0x02, 0x08, 0x9e, 0x7f, 0x3f };
     WRITE_READ(ctxitem0z, item19z, readdata13z);
 
