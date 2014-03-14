fb7a24b7aa2c6daf648a6819294e0ccec6ccd27c
John Ankcorn <jca@alum.mit.edu>
2014-03-14 07:58:13 -0400

    new executable for anchoring diffs

---
+++ librarywrap/munge.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# Copyright (c) 2013 Quanta Research Cambridge, Inc.
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use, copy,
+# modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import print_function
+import optparse, os, sys
+
+if __name__=='__main__':
+    parser = optparse.OptionParser("usage: %prog [options] arg")
+    parser.add_option("-l", "--library", dest="library")
+    (options, args) = parser.parse_args()
+    counter = 1000
+    for afile in args:
+        lines = open(afile).readlines()
+        for temp in lines:
+            temp = temp.rstrip();
+            if temp.find('0xcd') > 0:
+                for i in range(1000, 1040):
+                    print('BOZOMATCHLINEbefore', i, counter)
+                print(temp)
+                for i in range(1000, 1040):
+                    print('BOZOMATCHLINEafter', i, counter)
+                counter = counter + 1
+            else:
+                print(temp)
