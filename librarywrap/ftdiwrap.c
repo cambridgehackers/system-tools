0a3f6852b5a95658261cae1275f5bc8e67bc6f76
John Ankcorn <jca@alum.mit.edu>
2014-03-14 07:57:43 -0400

    change name of memdump to memdumpfile

--- librarywrap/ftdiwrap.c
+++ librarywrap/ftdiwrap.c
@@ -10,7 +10,7 @@
 
 #define ACCUM_LIMIT 0xfffffff //12000
 
-static void memdump(const unsigned char *p, int len, const char *title);
+static void memdumpfile(const unsigned char *p, int len, const char *title);
 static void dump_context(struct ftdi_context *p);
 static void final_dump(void);
 static char *writedata(int submit, const unsigned char *buf, int size);
@@ -26,6 +26,7 @@ static struct ftdi_transfer_control *write_data_submit_control;
 static int write_data_len;
 static long accum;
 static struct ftdi_context *master_ftdi;
+static int logall = 1;
 static int datafile_fd = -1;
 
 #include "ftdiwrap.h"
@@ -49,7 +50,6 @@ static struct {
     int len;
 } bufarr[MAX_ITEMS];
 static int bufarr_index = 0;
-static int logall = 1;
 
 static char *translate_context(struct ftdi_context *p)
 {
@@ -82,92 +82,7 @@ if (i == bufarr_index) {
 sprintf(tempstr, "bufitem%dz, sizeof(bufitem%dz)", i, i);
 return strdup(tempstr);
 }
-
-static void memdump(const unsigned char *p, int len, const char *title)
-{
-int i;
-
-    i = 0;
-    while (len > 0) {
-        if (!(i & 0xf)) {
-            if (i > 0)
-                fprintf(logfile, "\n");
-            fprintf(logfile, "%s ",title);
-        }
-        fprintf(logfile, "0x%02x, ", *p++);
-        i++;
-        len--;
-    }
-    fprintf(logfile, "\n");
-}
-static int started = 0;
-static void formatwrite(int submit, const unsigned char *p, int len, const char *title)
-{
-   static unsigned char bitswap[256];
-   static int once = 1;
-   static const char *header = "    ";
-   static char header_data[200];
-   if (logall)
-       header = "WRITE";
-   strcpy(header_data, header);
-   strcat(header_data, "   ");
-   
-    while (len > 0) {
-        const unsigned char *pstart = p;
-        int plen = 1;
-        unsigned char ch = *p;
-        switch(ch) {
-        case 0x85: case 0x87: case 0x8a: case 0xaa: case 0xab:
-            break;
-        case 0x2e:
-            plen = 2;
-            break;
-        case 0x19: case 0x1b: case 0x2c: case 0x3d: case 0x3f: case 0x4b:
-        case 0x6f: case 0x80: case 0x82: case 0x86: case 0x8f:
-            plen = 3;
-            break;
-        default:
-            memdump(p-1, len, title);
-            return;
-        }
-        if (!submit || accum < ACCUM_LIMIT)
-            memdump(pstart, plen, header);
-        if (started && p[0] == 0x1b && p[1] == 6)
-            write(datafile_fd, &bitswap[p[2]], 1);
-        p += plen;
-        len -= plen;
-        if (ch == 0x19 || ch == 0x3d) {
-            unsigned tlen = (pstart[2] << 8 | pstart[1]) + 1;
-if (tlen > 1500) {
-    started = 1;
-}
-else
-    started = 0;    // shutdown before final writes
-            if (!started)
-                memdump(p, tlen, header_data);
-            //if (submit && tlen > 4) 
-            else {
-                int i;
-                for (i = 0; once && i < sizeof(bitswap); i++)
-                    bitswap[i] = ((i &    1) << 7) | ((i &    2) << 5)
-                       | ((i &    4) << 3) | ((i &    8) << 1)
-                       | ((i & 0x10) >> 1) | ((i & 0x20) >> 3)
-                       | ((i & 0x40) >> 5) | ((i & 0x80) >> 7);
-                unsigned char *pbuf = (unsigned char *)malloc(tlen);
-                for (i = 0; i < tlen; i++)
-                    pbuf[i] = bitswap[p[i]];
-                write(datafile_fd, pbuf, tlen);
-                free(pbuf);
-                once = 0;
-            }
-            p += tlen;
-            len -= tlen;
-        }
-    }
-    if (len != 0)
-        printf("[%s] ending length %d\n", __FUNCTION__, len);
-}
-#define FILE_BYTE_SIZE  500
+#include "dumpdata.h"
 
 static char *writedata(int submit, const unsigned char *buf, int size)
 {
@@ -205,7 +120,7 @@ static char *readdata(const unsigned char *buf, int size)
     int ind = -1;
     int i = 0;
     if (logall)
-        memdump(buf, size, "READ");
+        memdumpfile(buf, size, "READ");
     else {
         while (i < readarr_index) {
             if (readarr[i].len == size && !memcmp(buf, readarr[i].p, size)) {
@@ -250,7 +165,7 @@ static void final_dump(void)
     i = 0;
     while (i < readarr_index) {
         fprintf(logfile, "static unsigned char readdata%dz[] = {\n", i);
-        memdump(readarr[i].p, readarr[i].len, "    ");
+        memdumpfile(readarr[i].p, readarr[i].len, "    ");
         fprintf(logfile, "};\n");
         i++;
     }
